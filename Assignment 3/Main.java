04ea6158bcd73cebfce812ed977949c62f445a83
Danial <48023732+dfx81@users.noreply.github.com>
2020-07-06 14:54:17 +0800

    Bug fix
    bug fix

--- Assignment 3/Main.java
+++ Assignment 3/Main.java
@@ -3,42 +3,47 @@ import java.io.File;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 class Main {
     File file = new File("./Records.txt");
     ArrayList <Airlines> record;
     Scanner in;
     
+    JFrame frame;
+    JLabel text;
+    
     void program() {
-        System.out.println("This is a list of Airlines:");
-        
         load();
         
+        frame = new JFrame("Airloka [PROTOTYPE]");
+        frame.setLayout(new BorderLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(640, 480);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        JPanel panel = new JPanel();
+        JButton add = new JButton("Add");
+        JButton delete = new JButton("Delete");
+        JButton edit = new JButton("Edit");
+        
+        add.addActionListener((e) -> add());
+        delete.addActionListener((e) -> remove());
+        edit.addActionListener((e) -> edit());
+        
+        text = new JLabel();
         display();
+        JScrollPane pane = new JScrollPane(text);
+        panel.add(add);
+        panel.add(delete);
+        panel.add(edit);
+        frame.add(panel, BorderLayout.SOUTH);
+        frame.add(pane, BorderLayout.CENTER);
         
-        in = new Scanner(System.in);
-        
-        boolean edit = true;
-        String next = "";
-        
-        while (edit) {
-            System.out.print("\nModifying record (type /add, /edit, /delete, or /exit):");
-            next = in.nextLine();
-            
-            switch (next) {
-                case "/exit": edit = false;
-                              break;
-                case "/add": add();
-                             break;
-                case "/edit": edit();
-                              break;
-                case "/delete": remove();
-                                break;
-                default: break;
-            }
-            
-            System.out.println();
-            display();
-        }
+        frame.setVisible(true);
         
         write();
         
@@ -98,76 +103,137 @@ class Main {
     
     void add() {
         try {
-            System.out.print("Enter name of the Airline to add: ");
-            String name = in.nextLine();
-            System.out.print("Enter rating of the Airline to add: ");
-            int rating = in.nextInt();
-            System.out.print("Enter the type of Airline [0: Reward, 1: Tier]:");
-            int type = in.nextInt();
-            in.nextLine();
-        
-            switch (type) {
-                case 0:
-                    record.add(new RewardAirlines(name, rating)); break;
-                case 1:
-                    record.add(new TierAirlines(name, rating)); break;
+            String name = (String)JOptionPane.showInputDialog(frame,
+            "Enter name of the Airline: ",
+            "Add",
+            JOptionPane.PLAIN_MESSAGE,
+            null,
+            null,
+            null);
+            
+            int rating = Integer.parseInt((String)JOptionPane.showInputDialog(frame,
+            "Enter rating of the Airline (0, 1, ...): ",
+            "Add",
+            JOptionPane.PLAIN_MESSAGE,
+            null,
+            null,
+            null));
+            
+            int type = Integer.parseInt((String)JOptionPane.showInputDialog(frame,
+            "Enter the type of Airline [0: Reward, 1: Tiered]: ",
+            "Add",
+            JOptionPane.PLAIN_MESSAGE,
+            null,
+            null,
+            null));
+            
+            if (type == 1) {
+                record.add(new TierAirlines(name, rating, 0));
             }
+        
+            else if (type == 0) {
+                record.add(new RewardAirlines(name, rating, 0));
+            }
+            
+            display();
+            
+            write();
         } catch (Exception err) {
-            System.out.println("Wrong input");
+            System.out.println("Wrong input" + err);
             System.exit(-3);
         }
     }
     
     void edit() {
         try {
-            System.out.print("Enter index of the Airline to edit: ");
-            int id = in.nextInt();
-            in.nextLine();
-        
-            System.out.print("Enter new name of the Airline (" + record.get(id).getName() + "): ");
-            String newName = in.nextLine();
-            System.out.print("Enter new rating of the Airline (" + record.get(id).getRating() + "): ");
-            int newRating = in.nextInt();
-            in.nextLine();
-        
+            int id = Integer.parseInt((String)JOptionPane.showInputDialog(frame,
+            "Enter index of the Airline to delete (0, 1, ...): ",
+            "Delete",
+            JOptionPane.PLAIN_MESSAGE,
+            null,
+            null,
+            null));
+            
+            String newName = (String)JOptionPane.showInputDialog(frame,
+            "Enter new name of the Airline: ",
+            "Edit",
+            JOptionPane.PLAIN_MESSAGE,
+            null,
+            null,
+            null);
+            
+            int newRating = Integer.parseInt((String)JOptionPane.showInputDialog(frame,
+            "Enter new rating of the Airline (0, 1, ...): ",
+            "Edit",
+            JOptionPane.PLAIN_MESSAGE,
+            null,
+            null,
+            null));
+            
             if (record.get(id).getType() == 1) {
-                System.out.print("Enter new Tier of the Airline (" + ((TierAirlines)record.get(id)).getTier() + "): ");
-                int newTier = in.nextInt();
+                int newTier = Integer.parseInt((String)JOptionPane.showInputDialog(frame,
+                "Enter new tier of the Tier Airline (0, 1, ...): ",
+                "Edit",
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                null,
+                null));
                 ((TierAirlines)record.get(id)).setTier(newTier);
             }
         
             if (record.get(id).getType() == 0) {
-                System.out.print("Enter new XP of the Airline (" + ((RewardAirlines)record.get(id)).getXP() + "): ");
-                double newXP = in.nextDouble();
+                double newXP = Double.parseDouble((String)JOptionPane.showInputDialog(frame,
+                "Enter new XP of the Reward Airline (0.0, 1.5, 4.5, ...): ",
+                "Edit",
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                null,
+                null));
                 ((RewardAirlines)record.get(id)).setXP(newXP);
             }
-        
+            
             record.get(id).setName(newName);
             record.get(id).setRating(newRating);
+            
+            display();
+            
+            write();
         } catch (Exception err) {
-            System.out.println("Wrong input");
+            System.out.println("Wrong input" + err);
             System.exit(-3);
         }
     }
     
     void remove() {
         try {
-            System.out.print("Enter index of the Airline to delete: ");
-            int id = in.nextInt();
-            in.nextLine();
+            int id = Integer.parseInt((String)JOptionPane.showInputDialog(frame,
+            "Enter index of the Airline to delete (0, 1, ...): ",
+            "Delete",
+            JOptionPane.PLAIN_MESSAGE,
+            null,
+            null,
+            null));
         
             record.remove(id);
+            
+            display();
+            
+            write();
         } catch (Exception err) {
-            System.out.println("Wrong input");
+            System.out.println("Wrong input" + err);
             System.exit(-3);
         }
     }
     
     void display() {
+        String str = "<html><p>";
         for (int i = 0; i != record.size(); i++) {
-            System.out.println(i + ": Name - " + record.get(i).getName() + " | Type - " + record.get(i).getType() + " | Rating - " + record.get(i).getRating()
-                + ((record.get(i).getType() == 0) ? " | XP - " + ((RewardAirlines)record.get(i)).getXP() : " | Tier - " + ((TierAirlines)record.get(i)).getTier()));
+            str += (i + ": Name - " + record.get(i).getName() + " | Type - " + record.get(i).getType() + " | Rating - " + record.get(i).getRating()
+                + ((record.get(i).getType() == 0) ? " | XP - " + ((RewardAirlines)record.get(i)).getXP() : " | Tier - " + ((TierAirlines)record.get(i)).getTier()) + "<br/>");
         }
+        
+        str += "</p></html>";
+        text.setText(str);
     }
     
     public static void main(String[] args) {
